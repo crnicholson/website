1eb0b8d9181996b396ff150022a106e1c938ff9f
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-05-26 16:55:08 -0400

    Fix Vercel error

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,10 +6,10 @@ export default function Hero() {
                 <div className="h-[10%]"></div>
                 <div className="h-[80%] flex flex-col justify-center items-center text-center">
                     <h1 className="sm:text-8xl text-5xl font-bold  text-[#e0e1dd]">
-                        Hi. I'm Charlie Nicholson.
+                        Hi. I{"'"}m Charlie Nicholson.
                     </h1>
                     <h1 className="sm:text-8xl text-5xl font-bold text-[#0d1b2a]">
-                        Hi. I'm Charlie Nicholson.
+                        Hi. I{"'"}m Charlie Nicholson.
                     </h1>
                     {/* <p className="text-2xl font-light text-white">
                         I'm an engineer of all sorts.
@@ -17,7 +17,7 @@ export default function Hero() {
                 </div>
                 <div className="h-[10%] flex flex-col justify-center items-center">
                     <a href="#about" className="bottom-5 italic font-light underline decoration-dotted hover:decoration-solid text-white">
-                        Psst! There's more.
+                        Psst! There{"'"}s more.
                     </a>
                 </div>
             </div>
