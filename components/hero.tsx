3dd21f7fcf2571ebc75f0720f98b3dd79f15671f
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-05-26 16:48:56 -0400

    Trying a font

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,14 +1,14 @@
 export default function Hero() {
     return (
-        <div className="relative bg-[#e0e1dd]">
+        <div className="relative bg-[#e0e1dd] font-[heming]">
             <div id="slash" className="ml-[-600px] absolute z-0 h-[100vh] w-[83vw] bg-[#0d1b2a]"></div>
             <div className="relative z-10 h-screen flex flex-col px-10">
                 <div className="h-[10%]"></div>
                 <div className="h-[80%] flex flex-col justify-center items-center text-center">
-                    <h1 className="sm:text-6xl text-5xl font-bold  text-[#e0e1dd]">
+                    <h1 className="sm:text-8xl text-5xl font-bold  text-[#e0e1dd]">
                         Hi. I'm Charlie Nicholson.
                     </h1>
-                    <h1 className="sm:text-6xl text-5xl font-bold text-[#0d1b2a]">
+                    <h1 className="sm:text-8xl text-5xl font-bold text-[#0d1b2a]">
                         Hi. I'm Charlie Nicholson.
                     </h1>
                     {/* <p className="text-2xl font-light text-white">
