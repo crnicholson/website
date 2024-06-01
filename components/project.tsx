2f6e7d0265348948600832274dcfef2cbd3cdec9
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-01 13:36:42 -0400

    Possible mobile fix

--- components/project.tsx
+++ components/project.tsx
@@ -2,8 +2,8 @@ export default function Project(props: { title: string; name: string; url: strin
     return (
         <>
             <a href={props.url} target="_blank">
-                <div className="relative group rounded-lg m-5 transform transition-transform duration-300 hover:scale-105 bg-[rgba(0,0,0,.1)] hover:shadow-2xl h-[300px] w-fit">
-                    <img className="rounded-lg w-full h-full object-contain" src={props.image} alt={props.title} />
+                <div className="relative group rounded-lg m-5 transform transition-transform duration-300 hover:scale-105 bg-[rgba(0,0,0,.1)] hover:shadow-2xl aspect-w-1 aspect-h-1 w-full sm:w-fit">
+                    <img className="rounded-lg w-full h-full object-cover" src={props.image} alt={props.title} />
                     <div className="w-fit mx-auto p-1 rounded-2xl italic absolute bottom-0 inset-x-0 flex flex-row mb-2 justify-center items-center" style={{ backgroundColor: props.bg }}>
                         <h1 id={props.name} className="mr-2 text-[13px] font-bold transition-colors duration-300" style={{ color: props.color }}>
                             {props.title}
@@ -13,7 +13,7 @@ export default function Project(props: { title: string; name: string; url: strin
                         </p>
                     </div>
                     <div className="absolute inset-0 shadow-2xl rounded-lg group-hover:border-transparent transition-all duration-300"></div>
-                </div>
+                </div>  
             </a>
         </>
     );
