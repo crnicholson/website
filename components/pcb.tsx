7d599f2216c67b00d78f28b7ad2f21c0ce27b221
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-20 09:53:14 -0400

    Revisions and board count at the bottom

--- components/pcb.tsx
+++ components/pcb.tsx
@@ -6,7 +6,7 @@ export default function MoreProjects(props: { content: string; image: string; li
                     <div className="group flex flex-col gap-2 items-center">
                         <div className="relative mx-5 my-2 transform transition-transform duration-300  bg-[rgba(0,0,0,.1)] group-hover:shadow-2xl h-[300px] w-fit rounded-lg">
                             <img className="rounded-lg w-fit h-[300px] group-hover:opacity-10" src={props.image} alt={props.title} />
-                            <div className="relative -mt-[296px] ml-2 w-[150px] group-hover:invisible" style={{ color: props.color }}>
+                            <div className="text-sm relative -mt-[296px] ml-2 w-[150px] group-hover:invisible" style={{ color: props.color }}>
                                 <p>{props.revisions} revisions, {props.boards} total boards</p>
                             </div>
                             <div className="absolute inset-0 shadow-2xl rounded-lg group-hover:border-transparent transition-all duration-300">
@@ -30,6 +30,11 @@ export default function MoreProjects(props: { content: string; image: string; li
                             <p className="text-[8px] bg-[rgba(0,0,0,0.1)] rounded-2xl p-[3px]">
                                 {props.date}
                             </p>
+                        </div>
+                        <div className="w-fit h-fit p-1 rounded-2xl italic inset-x-0 flex flex-row justify-center items-center" style={{ backgroundColor: props.bg }}>
+                            <h1 id={props.name} className="mr-2 text-[13px] font-bold transition-colors duration-300">
+                                <p>{props.revisions} revisions, {props.boards} total boards</p>
+                            </h1>
                         </div>
                     </div >
                 </a>
