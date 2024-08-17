96c415c1eeedb548106941c2a59d6e2be71145dd
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-08-17 10:21:38 -0400

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "Charlie Nicholson's Personal Website",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      <body className="">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
