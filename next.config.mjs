a3f756a27c072fb864de01b224fee6138e0070af
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-11-28 22:40:39 -0500

    Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+export default {
+  async redirects() {
+    return [
+      {
+        source: "/strato",
+        destination: "https://github.com/crnicholson/StratoSoar-MK3",
+        permanent: true, // 308 for permanent redirect
+      },
+    ];
+  },
+};
